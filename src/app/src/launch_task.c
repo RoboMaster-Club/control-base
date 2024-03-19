232623a0fbf94084622ce5adf8a1ee72a691b497
jia-xie <103150184+jia-xie@users.noreply.github.com>
2024-03-19 17:54:37 -0400

    launch works

--- src/app/src/launch_task.c
+++ src/app/src/launch_task.c
@@ -8,7 +8,7 @@
 extern Robot_State_t g_robot_state;
 extern Remote_t g_remote;
 extern IMU_t g_imu;
-DJI_Motor_Handle_t *g_flywheel_left, *g_flywheel_right, *g_feed_angle, *g_feed_speed;
+DJI_Motor_Handle_t *g_flywheel_left, *g_flywheel_right, *g_motor_feed, *g_motor_feed;
 Launch_Target_t g_launch_target;
 
 void Feed_Angle_Calc(void);
@@ -40,38 +40,30 @@ void Launch_Task_Init() {
             },
     };
 
-    // Motor_Config_t feed_angle_config = {
-    //     .can_bus = 2,
-    //     .speed_controller_id = 7,
-    //     .offset = 0,
-    //     .control_mode = POSITION_CONTROL,
-    //     .motor_reversal = MOTOR_REVERSAL_NORMAL,
-    //     .angle_pid =
-    //         {
-    //             .kp = 20000.0f,
-    //             .ki = 1.2f,
-    //             .output_limit = M2006_MAX_CURRENT,
-    //             .integral_limit = 1000.0f,
-    //         }
-    // };
-
     Motor_Config_t feed_speed_config = {
         .can_bus = 1,
-        .speed_controller_id = 3,
+        .speed_controller_id = 1,
         .offset = 0,
-        .control_mode = VELOCITY_CONTROL,
-        .motor_reversal = MOTOR_REVERSAL_NORMAL,
+        .control_mode = VELOCITY_CONTROL | POSITION_CONTROL,
+        .motor_reversal = MOTOR_REVERSAL_REVERSED,
         .velocity_pid =
             {
                 .kp = 500.0f,
                 .output_limit = M2006_MAX_CURRENT,
+            },
+        .angle_pid =
+            {
+                .kp = 400000.0f,
+                .kd = 15000000.0f,
+                .ki = 0.1f,
+                .output_limit = M2006_MAX_CURRENT,
+                .integral_limit = 1000.0f,
             }
     };
 
     g_flywheel_left = DJI_Motor_Init(&flywheel_left_config,M3508);
     g_flywheel_right = DJI_Motor_Init(&flywheel_right_config,M3508);
-    //g_feed_angle = DJI_Motor_Init(&feed_angle_config,M2006);
-    g_feed_speed = DJI_Motor_Init(&feed_speed_config,M2006);
+    g_motor_feed = DJI_Motor_Init(&feed_speed_config,M2006);
 }
 
 void Launch_Ctrl_Loop() {
@@ -89,8 +81,7 @@ void Launch_Ctrl_Loop() {
     } else {
         DJI_Motor_Disable(g_flywheel_left);
         DJI_Motor_Disable(g_flywheel_right);
-        //DJI_Motor_Disable(g_feed_angle);
-        DJI_Motor_Disable(g_feed_speed);
+        DJI_Motor_Disable(g_motor_feed);
     }
 }
 
@@ -109,17 +100,20 @@ void Feed_Angle_Calc()
     }
 
     if (g_launch_target.single_launch_flag && !g_launch_target.single_launch_finished_flag) {
-        g_launch_target.feed_angle = DJI_Motor_Get_Angle(g_feed_angle) + FEED_1_PROJECTILE_ANGLE;
+        g_launch_target.feed_angle = DJI_Motor_Get_Total_Angle(g_motor_feed) + FEED_1_PROJECTILE_ANGLE;
         g_launch_target.single_launch_finished_flag = 1;
     }
     else if (g_launch_target.single_launch_flag && g_launch_target.single_launch_finished_flag) {
-        //DJI_Motor_Set_Angle(g_feed_angle,g_launch_target.feed_angle);
+        DJI_Motor_Set_Control_Mode(g_motor_feed, POSITION_CONTROL_TOTAL_ANGLE);
+        DJI_Motor_Set_Angle(g_motor_feed,g_launch_target.feed_angle);
     }
     else if (g_launch_target.burst_launch_flag) {
-        g_launch_target.feed_velocity = FEED_FREQUENCY_18;
-        DJI_Motor_Set_Velocity(g_feed_speed,g_launch_target.feed_velocity);
+        g_launch_target.feed_velocity = FEED_FREQUENCY_6;
+        DJI_Motor_Set_Control_Mode(g_motor_feed, VELOCITY_CONTROL);
+        DJI_Motor_Set_Velocity(g_motor_feed,g_launch_target.feed_velocity);
     }
     else {
-        DJI_Motor_Set_Velocity(g_feed_speed,0);
+        DJI_Motor_Set_Control_Mode(g_motor_feed, VELOCITY_CONTROL);
+        DJI_Motor_Set_Velocity(g_motor_feed,0);
     }
 }
