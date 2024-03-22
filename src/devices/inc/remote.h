57b2bd25af7541b92a659b2c025c56790ccfb89f
jia-xie <103150184+jia-xie@users.noreply.github.com>
2024-03-22 17:07:01 -0400

    remote modify

--- src/devices/inc/remote.h
+++ src/devices/inc/remote.h
@@ -11,6 +11,23 @@
 
 #define REMOTE_STICK_MAX (660.0f)
 
+#define KEY_W ((uint16_t)0x01<<0)
+#define KEY_S ((uint16_t)0x01<<1)
+#define KEY_A ((uint16_t)0x01<<2)
+#define KEY_D ((uint16_t)0x01<<3)
+#define KEY_SHIFT ((uint16_t)0x01<<4)
+#define KEY_CTRL ((uint16_t)0x01<<5)   
+#define KEY_Q ((uint16_t)0x01<<6)      
+#define KEY_E ((uint16_t)0x01<<7)      
+#define KEY_R ((uint16_t)0x01<<8)    
+#define KEY_F ((uint16_t)0x01<<9)
+#define KEY_G ((uint16_t)0x01<<10)     
+#define KEY_Z ((uint16_t)0x01<<11)    
+#define KEY_X ((uint16_t)0x01<<12)
+#define KEY_C ((uint16_t)0x01<<13)
+#define KEY_V ((uint16_t)0x01<<14)
+#define KEY_B ((uint16_t)0x01<<15)  
+
 enum SwitchPos {UP=1, DOWN, MID};
 
 struct Joystick
@@ -38,13 +55,21 @@ struct Keyboard
     uint8_t E;
     uint8_t Shift;
     uint8_t Ctrl;
+    uint8_t R;
+    uint8_t F;
+    uint8_t G;
+    uint8_t Z;
+    uint8_t X;
+    uint8_t C;
+    uint8_t V;
+    uint8_t B;
 };
 
 struct Mouse
 {
-    uint16_t x;
-    uint16_t y;
-    uint16_t z;
+    int16_t x;
+    int16_t y;
+    int16_t z;
     uint8_t left;
     uint8_t right;
 };
