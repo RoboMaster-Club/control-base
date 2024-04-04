6c7286669f01af9541cb1d9b871cf8382e1a914f
jia-xie <103150184+jia-xie@users.noreply.github.com>
2024-04-04 18:08:17 -0400

    uart send based on type

--- src/bsp/Inc/bsp_uart.h
+++ src/bsp/Inc/bsp_uart.h
@@ -3,6 +3,10 @@
 
 #include "usart.h"
 
+#define UART_BLOCKING (0b00000000)
+#define UART_IT (0b00000001)
+#define UART_DMA (0b00000010)
+
 typedef struct _UART_Instance
 {
     UART_HandleTypeDef *uart_handle;
@@ -13,5 +17,5 @@ typedef struct _UART_Instance
 
 void UART_Service_Init(UART_Instance_t *uart_insatce);
 UART_Instance_t *UART_Register(UART_HandleTypeDef *huart, uint8_t *rx_buffer, uint8_t rx_buffer_size, void (*callback)(UART_Instance_t *uart_instance)); 
-
+HAL_StatusTypeDef UART_Transmit(UART_Instance_t *uart_instance, uint8_t *tx_buffer, uint16_t tx_buffer_size, uint8_t send_type);
 #endif // BSP_UART_H
