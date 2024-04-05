789a8f53d0981e66835506b8faf69127399b7572
jia-xie <103150184+jia-xie@users.noreply.github.com>
2024-04-05 11:55:21 -0400

    unitree motor update to current bsp_uart and add daemon

--- src/devices/inc/unitree_motor.h
+++ src/devices/inc/unitree_motor.h
@@ -3,6 +3,12 @@
 
 #include <stdint.h>
 #include "main.h"
+#include "bsp_uart.h"
+
+#define UNITREE_FOC_MODE (10)
+#define UNITREE_OPEN_LOOP_MODE (5)
+#define UNITREE_IDLE_MODE (0)
+#define UNITREE_TIMEOUT_MS (1000)
 
 /*
  * This mysterious table is just the CRC of each possible byte. It can be
@@ -65,48 +71,9 @@ static uint16_t crc_ccitt(uint16_t crc, uint8_t const *buffer, size_t len)
     return tmp;
 }
 
-#pragma pack(1)
-
-/**
- * @brief 电机模式控制信息
- *
+/** Motor Protocol Data Structure, Reference:
+ * https://support.unitree.com/home/en/Motor_SDK_Dev_Guide/communication_protocol
  */
-typedef struct
-{
-    uint8_t id : 4;     // 电机ID: 0,1...,13,14 15表示向所有电机广播数据(此时无返回)
-    uint8_t status : 3; // 工作模式: 0.锁定 1.FOC闭环 2.编码器校准 3.保留
-    uint8_t none : 1;   // 保留位
-} RIS_Mode_t;           // 控制模式 1Byte
-
-/**
- * @brief 电机状态控制信息
- *
- */
-typedef struct
-{
-    int16_t tor_des; // 期望关节输出扭矩 unit: N.m      (q8)
-    int16_t spd_des; // 期望关节输出速度 unit: rad/s    (q8)
-    int32_t pos_des; // 期望关节输出位置 unit: rad      (q15)
-    int16_t k_pos;   // 期望关节刚度系数 unit: -1.0-1.0 (q15)
-    int16_t k_spd;   // 期望关节阻尼系数 unit: -1.0-1.0 (q15)
-
-} RIS_Comd_t; // 控制参数 12Byte
-
-/**
- * @brief 电机状态反馈信息
- *
- */
-typedef struct
-{
-    int16_t torque;      // 实际关节输出扭矩 unit: N.m     (q8)
-    int16_t speed;       // 实际关节输出速度 unit: rad/s   (q8)
-    int32_t pos;         // 实际关节输出位置 unit: rad     (q15)
-    int8_t temp;         // 电机温度: -128~127°C
-    uint8_t MError : 3;  // 电机错误标识: 0.正常 1.过热 2.过流 3.过压 4.编码器故障 5-7.保留
-    uint16_t force : 12; // 足端气压传感器数据 12bit (0-4095)
-    uint8_t none : 1;    // 保留位
-} RIS_Fbk_t;             // 状态数据 11Byte
-
 #pragma pack(1)
 typedef union
 {
@@ -119,153 +86,97 @@ typedef union
 
 typedef struct
 {
-    // 定义 数据包头
-    unsigned char start[2]; // 包头
-    unsigned char motorID;  // 电机ID  0,1,2,3 ...   0xBB 表示向所有电机广播（此时无返回）
-    unsigned char reserved;
-} COMHead;
+    uint8_t head[2]; // Header         2Byte
+    struct
+    {
+        uint8_t id : 4;   // Motor ID: 0,1...,13,14 15 represents broadcasting data to all motors (no return at this time)
+        uint8_t mode : 3; // Working mode: 0.Locked 1.FOC closed-loop 2.Encoder calibration 3.Reserved
+        uint8_t none : 1; // Reserved bit
+    } id_mode;
 
-typedef struct
-{ // 以 4个字节一组排列 ，不然编译器会凑整
-    // 定义 数据
-    uint8_t mode;    // 当前关节模式
-    uint8_t ReadBit; // 电机控制参数修改     是否成功位
-    int8_t Temp;     // 电机当前平均温度
-    uint8_t MError;  // 电机错误 标识
+    struct
+    {
+        int16_t torque;         // Actual joint output torque unit: N.m     (q8)
+        int16_t speed;          // Actual joint output speed unit: rad/s    (q8)
+        int32_t position;       // Actual joint output position unit: rad   (q15)
+        int8_t temp;            // Motor temperature: -128~127°C
+        uint8_t error_code : 3; // Motor error code: 0.Normal 1.Overheat 2.Overcurrent 3.Overvoltage 4.Encoder fault 5-7.Reserved
+        uint16_t force : 12;    // Foot pressure sensor data 12bit (0-4095)
+        uint8_t none : 1;       // Reserved bit
+    } feedback;                 // Motor feedback data 11Byte
 
-    COMData32 Read; // 读取的当前 电机 的控制数据
-    int16_t T;      // 当前实际电机输出力矩       7 + 8 描述
+    uint16_t CRC16; // CRC          2Byte
 
-    int16_t W; // 当前实际电机速度（高速）   8 + 7 描述
-    float LW;  // 当前实际电机速度（低速）
-
-    int16_t W2; // 当前实际关节速度（高速）   8 + 7 描述
-    float LW2;  // 当前实际关节速度（低速）
-
-    int16_t Acc;    // 电机转子加速度       15+0 描述  惯量较小
-    int16_t OutAcc; // 输出轴加速度         12+3 描述  惯量较大
-
-    int32_t Pos;  // 当前电机位置（主控0点修正，电机关节还是以编码器0点为准）
-    int32_t Pos2; // 关节编码器位置(输出编码器)
-
-    int16_t gyro[3]; // 电机驱动板6轴传感器数据
-    int16_t acc[3];
-
-    // 力传感器的数据
-    int16_t Fgyro[3];
-    int16_t Facc[3];
-    int16_t Fmag[3];
-    uint8_t Ftemp; // 8位表示的温度  7位（-28~100度）  1位0.5度分辨率
-
-    int16_t Force16; // 力传感器高16位数据
-    int8_t Force8;   // 力传感器低8位数据
-
-    uint8_t FError; //  足端传感器错误标识
-
-    int8_t Res[1]; // 通讯 保留字节
-
-} ServoComdV3; // 加上数据包的包头 和CRC 78字节（4+70+4）
+} Unitree_Motor_Rx_Buffer_t; // Return data
 
 typedef struct
 {
-    uint8_t head[2]; // 包头         2Byte
-    RIS_Mode_t mode; // 电机控制模式  1Byte
-    RIS_Fbk_t fbk;   // 电机反馈数据 11Byte
-    uint16_t CRC16;  // CRC          2Byte
-} MotorData_t;       // 返回数据
+    // Define motor control command data package
+    uint8_t head[2]; // Header         2Byte
 
-typedef struct
-{
-    uint8_t none[8]; // 保留
+    struct
+    {
+        uint8_t id : 4;   // Motor ID: 0,1...,13,14 15 represents broadcasting data to all motors (no return at this time)
+        uint8_t mode : 3; // Working mode: 0.Idle 1.FOC closed-loop 2.Encoder calibration 3.Reserved
+        uint8_t none : 1; // Reserved bit
+    } id_mode;            // Motor control mode  1Byte
 
-} LowHzMotorCmd;
+    struct
+    {
+        int16_t tor_des; // Desired joint output torque unit: N.m      (q8)
+        int16_t spd_des; // Desired joint output speed unit: rad/s    (q8)
+        int32_t pos_des; // Desired joint output position unit: rad      (q15)
+        int16_t k_pos;   // Desired joint stiffness coefficient unit: -1.0-1.0 (q15)
+        int16_t k_spd;   // Desired joint damping coefficient unit: -1.0-1.0 (q15)
 
-typedef struct
-{                      // 以 4个字节一组排列 ，不然编译器会凑整
-                       // 定义 数据
-    uint8_t mode;      // 关节模式选择
-    uint8_t ModifyBit; // 电机控制参数修改位
-    uint8_t ReadBit;   // 电机控制参数发送位
-    uint8_t reserved;
+    } cmd;
 
-    COMData32 Modify; // 电机参数修改 的数据
-    // 实际给FOC的指令力矩为：
-    //  K_P*delta_Pos + K_W*delta_W + T
-    int16_t T;   // 期望关节的输出力矩（电机本身的力矩）x256, 7 + 8 描述
-    int16_t W;   // 期望关节速度 （电机本身的速度） x128,       8 + 7描述
-    int32_t Pos; // 期望关节位置 x 16384/6.2832, 14位编码器（主控0点修正，电机关节还是以编码器0点为准）
-
-    int16_t K_P; // 关节刚度系数 x2048  4+11 描述
-    int16_t K_W; // 关节速度系数 x1024  5+10 描述
-
-    uint8_t LowHzMotorCmdIndex; // 保留
-    uint8_t LowHzMotorCmdByte;  // 保留
-
-    COMData32 Res[1]; // 通讯 保留字节  用于实现别的一些通讯内容
-
-} MasterComdV3; // 加上数据包的包头 和CRC 34字节
-
-typedef struct
-{
-    // 定义 电机控制命令数据包
-    uint8_t head[2]; // 包头         2Byte
-    RIS_Mode_t mode; // 电机控制模式  1Byte
-    RIS_Comd_t comd; // 电机期望数据 12Byte
-    uint16_t CRC16;  // CRC          2Byte
-} ControlData_t;     // 电机控制命令数据包
+    uint16_t CRC16;        // CRC          2Byte
+} Unitree_Motor_Tx_Buffer; // Motor control command data package
 
 #pragma pack()
 
 typedef struct
 {
-    // 定义 发送格式化数据
-    ControlData_t motor_send_data; // 电机控制数据结构体
-    int hex_len;                   // 发送的16进制命令数组长度, 34
-    long long send_time;           // 发送该命令的时间, 微秒(us)
-    // 待发送的各项数据
-    unsigned short id;   // 电机ID，0代表全部电机
-    unsigned short mode; // 0:空闲, 5:开环转动, 10:闭环FOC控制
-    // 实际给FOC的指令力矩为：
-    //  K_P*delta_Pos + K_W*delta_W + T
-    float T;       // 期望关节的输出力矩（电机本身的力矩）（Nm）
-    float W;       // 期望关节速度（电机本身的速度）(rad/s)
-    float Pos;     // 期望关节位置（rad）
-    float K_P;     // 关节刚度系数
-    float K_W;     // 关节速度系数
-    COMData32 Res; // 通讯 保留字节  用于实现别的一些通讯内容
-} MOTOR_send;
+    // Define formatted data to be sent
+    Unitree_Motor_Tx_Buffer tx_buffer; // Motor control data structure
+    int hex_len;                       // Length of the hexadecimal command array to be sent, 34
+    long long send_time;               // Time when the command is sent, in microseconds (us)
+    // Data to be sent
+    unsigned short id;   // Motor ID, 0 represents all motors
+    unsigned short mode; // 0: Idle, 5: Open-loop rotation, 10: Closed-loop FOC control
+    float torque;   // Desired joint output torque (motor's own torque) (Nm)
+    float velocity; // Desired joint velocity (motor's own velocity) (rad/s)
+    float position; // Desired joint position (rad)
+    float k_pos;    // Joint stiffness coefficient
+    float k_vel;    // Joint velocity coefficient
+    COMData32 Res;  // Communication reserved bytes, used for other communication content implementation
+} Unitree_Motor_Cmd_Package_t;
 
 typedef struct
 {
-    // 定义 接收数据
-    MotorData_t motor_recv_data; // 电机接收数据结构体，详见motor_msg.h
-    int hex_len;                 // 接收的16进制命令数组长度, 78
-    long long resv_time;         // 接收该命令的时间, 微秒(us)
-    int correct;                 // 接收数据是否完整（1完整，0不完整）
-    // 解读得出的电机数据
-    unsigned char motor_id; // 电机ID
-    unsigned char mode;     // 0:空闲, 5:开环转动, 10:闭环FOC控制
-    int Temp;               // 温度
-    unsigned char MError;   // 错误码
-    float T;                // 当前实际电机输出力矩
-    float W;                // speed
-    float Pos;              // 当前电机位置（主控0点修正，电机关节还是以编码器0点为准）
-    float footForce;        // 足端气压传感器数据 12bit (0-4095)
+    int hex_len;         // Length of the hexadecimal command array received, 78
+    long long resv_time; // Time when the command is received, in microseconds (us)
+    int correct;         // Whether the received data is complete (1 for complete, 0 for incomplete)
+    // Motor data obtained from interpretation
+    unsigned char motor_id;   // Motor ID
+    unsigned char mode;       // 0: Idle, 5: Open-loop rotation, 10: Closed-loop FOC control
+    int Temp;                 // Temperature
+    unsigned char error_code; // Error code
+    float torque;             // Current actual motor output torque
+    float velocity;           // speed
+    float position;           // Current motor position (based on encoder zero point correction, the motor joint is based on the encoder zero point)
+    float foot_force;         // Foot pressure sensor data 12bit (0-4095)
 
-} MOTOR_recv;
+} Unitree_Motor_Stats_t;
 
 typedef struct _Unitree_Motor
 {
-    uint8_t uart;
+    Unitree_Motor_Cmd_Package_t cmd;
+    Unitree_Motor_Stats_t stats;
+    UART_Instance_t *uart_instance;
     uint8_t motor_id;
-    MOTOR_send motor_send;
-    MOTOR_recv motor_recv;
-} Unitree_Motor;
-
-uint32_t crc32_core(uint32_t *ptr, uint32_t len);
-int modify_data(MOTOR_send *motor_s);
-int extract_data(MOTOR_recv *motor_r);
-HAL_StatusTypeDef SERVO_Send_recv(MOTOR_send *pData, MOTOR_recv *rData);
+} Unitree_Motor_t;
 
 #pragma pack()
 
--- src/devices/src/unitree_motor.c
+++ src/devices/src/unitree_motor.c
@@ -1,167 +1,170 @@
+#pragma message "TODO: add transmission reduction ratio support, otherwise, the all parameters are for the motor rotor side."
+#pragma message "TODO: add multiple uart support"
 #include "unitree_motor.h"
-#include "usart.h"
-#include "bsp_uart.h"
-#include "motor.h"
 
-#define SATURATE(_IN, _MIN, _MAX) \
-    {                             \
-        if (_IN < _MIN)           \
-            _IN = _MIN;           \
-        else if (_IN > _MAX)      \
-            _IN = _MAX;           \
-    }
-#define UNITREE_MOTOR_NUM 4
-Unitree_Motor *g_unitree_motor[UNITREE_MOTOR_NUM];
+#include "bsp_daemon.h"
+#include "user_math.h"
+#include <stdlib.h>
 
-int modify_data(MOTOR_send *motor_s)
+#define UNITREE_MOTOR_NUM (4)
+
+/******** TODO: add multiple uart support *******/
+Unitree_Motor_t *g_unitree_motors_ptr[UNITREE_MOTOR_NUM];
+Daemon_Instance_t *g_unitree_motor_daemon_ptr;
+UART_Instance_t *g_unitree_motor_uart_instance_ptr;
+/******** TODO: add multiple uart support *******/
+
+uint8_t g_unitree_motor_num = 0;
+uint8_t g_unitree_motor_service_initialzed = 0;
+uint8_t g_unitree_motor_statsx_buffer[sizeof(Unitree_Motor_Rx_Buffer_t)];
+
+/* Pricate Function and Callback Function Prototype */
+void Unitree_Motor_Callback(UART_Instance_t *uart_instance);
+void Unitree_Motor_Timeout_Callback(void);
+int _Unitree_Motor_Decode(Unitree_Motor_Rx_Buffer_t *motor_data);
+int _Unitree_Motor_Transform_Cmd(Unitree_Motor_Cmd_Package_t *cmd);
+
+/* Function Definition */
+
+/**
+ * @brief Initialize Unitree Motor Motor
+ * @details Initialize uart service and daemon protection if this is
+ * the first motor to be initialized on this uart instance.
+ *
+ * @param huartx UART_HandleTypeDef instance for the motor
+ * @param motor_id Motor ID (0-14)
+ *
+ * @return Unitree_Motor_t* Motor instance
+ */
+Unitree_Motor_t *Unitree_Motor_Init(UART_HandleTypeDef *huartx, uint8_t motor_id, float k_pos, float k_vel)
 {
-    motor_s->hex_len = 17;
-    motor_s->motor_send_data.head[0] = 0xFE;
-    motor_s->motor_send_data.head[1] = 0xEE;
+    if (g_unitree_motor_service_initialzed == 0)
+    {
+        g_unitree_motor_uart_instance_ptr = UART_Register(huartx, g_unitree_motor_statsx_buffer,
+                                                          sizeof(g_unitree_motor_statsx_buffer), Unitree_Motor_Callback);
+        uint16_t reload_value = UNITREE_TIMEOUT_MS / DAEMON_PERIOD;
+        uint16_t initial_counter = reload_value;
+        g_unitree_motor_daemon_ptr = Daemon_Register(reload_value, initial_counter, Unitree_Motor_Timeout_Callback);
+    }
+    Unitree_Motor_t *motor = (Unitree_Motor_t *)malloc(sizeof(Unitree_Motor_t));
+    motor->motor_id = motor_id;
+    motor->cmd.id = motor_id;
+    motor->cmd.mode = UNITREE_IDLE_MODE;
+    motor->cmd.k_pos = k_pos;
+    motor->cmd.k_vel = k_vel;
+    motor->uart_instance = g_unitree_motor_uart_instance_ptr;
+    
+    g_unitree_motors_ptr[g_unitree_motor_num++] = motor;
+    return motor;
+}
 
-    //		SATURATE(motor_s->id,   0,    15);
-    //		SATURATE(motor_s->mode, 0,    7);
-    SATURATE(motor_s->K_P, 0.0f, 25.599f);
-    SATURATE(motor_s->K_W, 0.0f, 25.599f);
-    SATURATE(motor_s->T, -127.99f, 127.99f);
-    SATURATE(motor_s->W, -804.00f, 804.00f);
-    SATURATE(motor_s->Pos, -411774.0f, 411774.0f);
+/**
+ * @brief Set torque to the motor. Will use the last FOC control parameters.
+ * or the default FOC control parameters when initialized.
+ * 
+ * @param motor Motor instance
+ * @param torque Desired joint output torque (Nm)
+ * 
+*/
+HAL_StatusTypeDef Unitree_Motor_Set_Torque(Unitree_Motor_t *motor, float torque)
+{
+    motor->cmd.torque = torque;
+    _Unitree_Motor_Transform_Cmd(&motor->cmd);
+    return UART_Transmit(motor->uart_instance, (uint8_t *)&motor->cmd, sizeof(motor->cmd), UART_DMA);
+}
 
-    motor_s->motor_send_data.mode.id = motor_s->id;
-    motor_s->motor_send_data.mode.status = motor_s->mode;
-    motor_s->motor_send_data.comd.k_pos = motor_s->K_P / 25.6f * 32768;
-    motor_s->motor_send_data.comd.k_spd = motor_s->K_W / 25.6f * 32768;
-    motor_s->motor_send_data.comd.pos_des = motor_s->Pos / 6.2832f * 32768;
-    motor_s->motor_send_data.comd.spd_des = motor_s->W / 6.2832f * 256;
-    motor_s->motor_send_data.comd.tor_des = motor_s->T * 256;
-    motor_s->motor_send_data.CRC16 = crc_ccitt(0, (uint8_t *)&motor_s->motor_send_data, 15);
+/**
+ * @brief Set the FOC control parameters.
+ * The actual torque command given to FOC is:
+ * k_pos*delta_pos + k_vel*delta_velocity + torque
+ * 
+ * @param motor Motor instance
+ * @param k_pos Joint stiffness coefficient
+ * @param k_vel Joint velocity coefficient
+ * @param position Desired joint position (rad)
+ * @param velocity Desired joint velocity (rad/s)
+ * @param torque Desired joint output torque (Nm)
+ * 
+*/
+HAL_StatusTypeDef Unitree_Motor_FOC(Unitree_Motor_t *motor, float k_pos, float k_vel, float position, float velocity, float torque)
+{
+    motor->cmd.k_pos = k_pos;
+    motor->cmd.k_vel = k_vel;
+    motor->cmd.position = position;
+    motor->cmd.velocity = velocity;
+    motor->cmd.torque = torque;
+    _Unitree_Motor_Transform_Cmd(&motor->cmd);
+    return UART_Transmit(motor->uart_instance, (uint8_t *)&motor->cmd, sizeof(motor->cmd), UART_DMA);
+}
+
+int _Unitree_Motor_Transform_Cmd(Unitree_Motor_Cmd_Package_t *cmd)
+{
+    cmd->hex_len = 17; // TODO: might be redundant
+    cmd->tx_buffer.head[0] = 0xFE;
+    cmd->tx_buffer.head[1] = 0xEE;
+
+    /** TODO: Change this to error triggering **/
+    // __MAX_LIMIT(cmd->id, 0, 15);
+    // __MAX_LIMIT(cmd->mode, 0, 7);
+    __MAX_LIMIT(cmd->k_pos, 0.0f, 25.599f);
+    __MAX_LIMIT(cmd->k_vel, 0.0f, 25.599f);
+    __MAX_LIMIT(cmd->torque, -127.99f, 127.99f);
+    __MAX_LIMIT(cmd->velocity, -804.00f, 804.00f);
+    __MAX_LIMIT(cmd->position, -411774.0f, 411774.0f);
+
+    cmd->tx_buffer.id_mode.id = cmd->id;
+    cmd->tx_buffer.id_mode.mode = cmd->mode;
+    cmd->tx_buffer.cmd.k_pos = cmd->k_pos / 25.6f * 32768;
+    cmd->tx_buffer.cmd.k_spd = cmd->k_vel / 25.6f * 32768;
+    cmd->tx_buffer.cmd.pos_des = cmd->position / 6.2832f * 32768;
+    cmd->tx_buffer.cmd.spd_des = cmd->velocity / 6.2832f * 256;
+    cmd->tx_buffer.cmd.tor_des = cmd->torque * 256;
+    cmd->tx_buffer.CRC16 = crc_ccitt(0, (uint8_t *)&cmd->tx_buffer, 15);
     return 0;
 }
 
-int Unitree_Motor_Decode(MOTOR_recv *motor_r)
+int _Unitree_Motor_Decode(Unitree_Motor_Rx_Buffer_t *motor_data)
 {
-    if (motor_r->motor_recv_data.CRC16 !=
-        crc_ccitt(0, (uint8_t *)&motor_r->motor_recv_data, 14))
+    if (motor_data->CRC16 !=
+        crc_ccitt(0, (uint8_t *)&motor_data, 14))
     {
-        // printf("[WARNING] Receive data CRC error");
-        motor_r->correct = 0;
-        return motor_r->correct;
-    }
-    else
-    {
-        motor_r->motor_id = motor_r->motor_recv_data.mode.id;
-        motor_r->mode = motor_r->motor_recv_data.mode.status;
-        motor_r->Temp = motor_r->motor_recv_data.fbk.temp;
-        motor_r->MError = motor_r->motor_recv_data.fbk.MError;
-        motor_r->W = ((float)motor_r->motor_recv_data.fbk.speed / 256) * 6.2832f;
-        motor_r->T = ((float)motor_r->motor_recv_data.fbk.torque) / 256;
-        motor_r->Pos = 6.2832f * ((float)motor_r->motor_recv_data.fbk.pos) / 32768;
-        motor_r->footForce = motor_r->motor_recv_data.fbk.force;
-        motor_r->correct = 1;
-        return motor_r->correct;
-    }
-}
-
-HAL_StatusTypeDef SERVO_Send_recv(MOTOR_send *pData, MOTOR_recv *rData)
-{
-    uint16_t rxlen = 0;
-
-    modify_data(pData);
-
-    HAL_UART_Transmit(&huart1, (uint8_t *)pData, sizeof(pData->motor_send_data), 10);
-
-    HAL_UARTEx_ReceiveToIdle(&huart1, (uint8_t *)rData, sizeof(rData->motor_recv_data), &rxlen, 10);
-
-    if (rxlen == 0)
-
-        return HAL_TIMEOUT;
-
-    if (rxlen != sizeof(rData->motor_recv_data))
-        return HAL_ERROR;
-
-    uint8_t *rp = (uint8_t *)&rData->motor_recv_data;
-    if (rp[0] == 0xFE && rp[1] == 0xEE)
-    {
-        rData->correct = 1;
-        extract_data(rData);
-        return HAL_OK;
+        /******* TODO: Add CRC Error Log ******/
+        return 1;
     }
 
-    return HAL_ERROR;
-}
-
-HAL_StatusTypeDef Unitree_Motor_Set_Torque(Unitree_Motor *motor, float torque)
-{
-    motor->motor_send.T = torque;
-    modify_data(&motor->motor_send);
-    return HAL_UART_Transmit_DMA(&huart1, (uint8_t *)&motor->motor_send, sizeof(motor->motor_send));
-}
-
-void Unitree_Motor_Init(Unitree_Motor *motor, uint8_t uart, uint8_t motor_id)
-{
-    motor->uart = uart;
-    motor->motor_id = motor_id;
-    motor->motor_send.id = motor_id;
-    motor->motor_send.mode = 10;
-    motor->motor_send.K_P = 0.0f;
-    motor->motor_send.K_W = 0.0f;
-    motor->motor_send.Pos = 0.0f;
-    motor->motor_send.W = 0.0f;
-    motor->motor_send.T = 0.0f;
-    motor->motor_send.Res.L = 0;
-    motor->motor_send.send_time = 0;
-    motor->motor_send.hex_len = 0;
-
-    motor->motor_recv.motor_id = motor_id;
-    motor->motor_recv.mode = 0;
-    motor->motor_recv.Temp = 0;
-    motor->motor_recv.MError = 0;
-    motor->motor_recv.T = 0.0f;
-    motor->motor_recv.W = 0.0f;
-    motor->motor_recv.Pos = 0.0f;
-    motor->motor_recv.footForce = 0;
-    motor->motor_recv.resv_time = 0;
-    motor->motor_recv.correct = 0;
-    motor->motor_recv.hex_len = 0;
-
-    for (int i = 0; i < UNITREE_MOTOR_NUM; i++)
+    for (int i = 0; i < g_unitree_motor_num; i++)
     {
-        if (g_unitree_motor[i] == NULL)
+        if (g_unitree_motors_ptr[i]->motor_id == motor_data->id_mode.id)
         {
-            g_unitree_motor[i] = motor;
+            Unitree_Motor_Stats_t *motor_stats = &(g_unitree_motors_ptr[i]->stats);
+            motor_stats->mode = motor_data->id_mode.mode;
+            motor_stats->Temp = motor_data->feedback.temp;
+            motor_stats->error_code = motor_data->feedback.error_code;
+            motor_stats->velocity = ((float)motor_data->feedback.speed / 256) * 6.2832f;
+            motor_stats->torque = ((float)motor_data->feedback.torque) / 256;
+            motor_stats->position = 6.2832f * ((float)motor_data->feedback.position) / 32768;
+            motor_stats->foot_force = motor_data->feedback.force;
+            return 0;
             break;
         }
     }
-}
-
-HAL_StatusTypeDef Unitree_Motor_FOC(Unitree_Motor *motor, float K_P, float K_W, float Pos, float W, float T)
-{
-    motor->motor_send.K_P = K_P;
-    motor->motor_send.K_W = K_W;
-    motor->motor_send.Pos = Pos;
-    motor->motor_send.W = W;
-    motor->motor_send.T = T;
-    modify_data(&motor->motor_send);
-    return HAL_UART_Transmit_DMA(&huart1, (uint8_t *)&motor->motor_send, sizeof(motor->motor_send));
-}
-
-HAL_StatusTypeDef Unitree_Motor_FOC_Send(Unitree_Motor *motor)
-{
-    modify_data(&motor->motor_send);
-    return HAL_UART_Transmit_DMA(&huart1, (uint8_t *)&motor->motor_send, sizeof(motor->motor_send));
+    // If reach here, motor ID is not found
+    // TODO: Add Motor ID Error Log
+    return 1;
 }
 
 void Unitree_Motor_Callback(UART_Instance_t *uart_instance)
 {
-    uint8_t *rp = uart_instance->rx_buffer;
-    if (rp[0] == 0xFE && rp[1] == 0xEE)
+    uint8_t *motor_rx_buffer = uart_instance->rx_buffer;
+    // Check Header
+    if (motor_rx_buffer[0] == 0xFE && motor_rx_buffer[1] == 0xEE)
     {
-        extract_data(rp);
-        return HAL_OK;
+        _Unitree_Motor_Decode((Unitree_Motor_Rx_Buffer_t *)motor_rx_buffer);
     }
+}
 
-    return HAL_ERROR;
-
-    extract_data(uart_instance->rx_buffer);
-
+void Unitree_Motor_Timeout_Callback()
+{
+    UART_Service_Init(g_unitree_motor_uart_instance_ptr);
+    // TODO: Timeout Error Log
 }
