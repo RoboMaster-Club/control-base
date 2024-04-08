f4a3b8956b8134fb4fc6de5732d343578ded4760
jia-xie <103150184+jia-xie@users.noreply.github.com>
2024-04-07 21:12:04 -0400

    add mf motor and dm motor support

--- src/app/src/motor_task.c
+++ src/app/src/motor_task.c
@@ -1,7 +1,11 @@
 #include "motor_task.h"
 #include "dji_motor.h"
+#include "dm_motor.h"
+#include "mf_motor.h"
 
 void Motor_Task_Loop() {
     DJI_Motor_Send();
+    MF_Motor_Send();
+    DM_Motor_Send();
 }
 
