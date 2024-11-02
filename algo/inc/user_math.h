aa46931b855f5a85595548079176d07f00159cbb
Irving <irvingywang@gmail.com>
2024-11-02 13:39:35 -0500

    Swerve Abstraction (#76)
    * refactor swerve draft
    
    removed motors
    
    * add conversions
    
    Co-Authored-By: Amanjyoti Mridha <71572787+QuackingBob@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Amanjyoti Mridha <71572787+QuackingBob@users.noreply.github.com>

--- algo/inc/user_math.h
+++ algo/inc/user_math.h
@@ -2,6 +2,7 @@
 #define USER_MATH_H
 
 #define PI (3.1415926f)
+#define PI_OVER_2 (PI / 2.0f)
 
 #define __MAX_LIMIT(val, min, max)     \
     do                                 \
@@ -40,19 +41,33 @@
     } while (0);
 
 #define BUFFER_SIZE (500)
-#define __MOVING_AVERAGE(buffer, index, update_value, count, sum, average)  \
-    do                                                                      \
-    {                                                                       \
-    if (count < BUFFER_SIZE) {                                              \
-        buffer[index] = update_value;                                       \
-        sum += update_value;                                                \
-        (count)++;                                                          \
-    } else {                                                                \
-        sum -= buffer[index];                                               \
-        buffer[index] = update_value;                                       \
-        sum += update_value;                                                \
-    }                                                                       \
-    index = (index + 1) % BUFFER_SIZE;                                      \
-    average = sum / count;                                                  \
+#define __MOVING_AVERAGE(buffer, index, update_value, count, sum, average) \
+    do                                                                     \
+    {                                                                      \
+        if (count < BUFFER_SIZE)                                           \
+        {                                                                  \
+            buffer[index] = update_value;                                  \
+            sum += update_value;                                           \
+            (count)++;                                                     \
+        }                                                                  \
+        else                                                               \
+        {                                                                  \
+            sum -= buffer[index];                                          \
+            buffer[index] = update_value;                                  \
+            sum += update_value;                                           \
+        }                                                                  \
+        index = (index + 1) % BUFFER_SIZE;                                 \
+        average = sum / count;                                             \
     } while (0);
-#endif
+
+#define __IS_TOGGLED(input, prev_input) \
+    (input == 1 && prev_input == 0)
+
+// transition means go from a different state to a target state
+#define __IS_TRANSITIONED(input, prev_input, transition_val) \
+    ((input == transition_val) && (prev_input != transition_val))
+
+#define __SLEW_RATE_LIMIT(curr, delta, ramp) \
+    curr = (1 - (ramp)) * (curr) + (ramp) * (delta)
+
+#endif // USER_MATH_H
---
+++ algo/src/swerve_locomotion.c
@@ -0,0 +1,126 @@
+#include "swerve_locomotion.h"
+
+swerve_constants_t swerve_init(float track_width, float wheel_base, float wheel_diameter, float max_speed, float max_angular_speed)
+{
+    swerve_constants_t swerve_constants = {
+        .track_width = track_width,
+        .wheel_base = wheel_base,
+        .wheel_diameter = wheel_diameter,
+        .max_speed = max_speed,
+        .max_angular_speed = max_angular_speed,
+        .kinematics_matrix = {
+            {0, 1, -(wheel_base / 2)}, // front left
+            {1, 0, -(track_width / 2)},
+            {0, 1, -(wheel_base / 2)}, // rear left
+            {1, 0, +(track_width / 2)},
+            {0, 1, +(wheel_base / 2)}, // rear right
+            {1, 0, +(track_width / 2)},
+            {0, 1, +(wheel_base / 2)}, // front right
+            {1, 0, -(track_width / 2)}}
+    };
+
+    return swerve_constants;
+}
+
+void calculate_swerve_kinematics(swerve_chassis_state_t *chassis_state, swerve_constants_t *swerve_constants) {
+    float v_x = chassis_state->v_x;
+    float v_y = chassis_state->v_y;
+    float omega = chassis_state->omega;
+    float(*k_mat)[3] = swerve_constants->kinematics_matrix;
+
+    if (v_x == 0 && v_y == 0 && omega == 0)
+    {
+        for (int i = 0; i < NUMBER_OF_MODULES; i++)
+        {
+            chassis_state->states[i].speed = 0;
+            chassis_state->states[i].angle = chassis_state->states[i].last_angle;
+        }
+        return;
+    }
+
+    // Multiply the inverse kinematics matrix by the chassis speeds vector
+    float module_states_matrix[8] = {
+        k_mat[0][0] * v_x + k_mat[0][1] * v_y + k_mat[0][2] * omega,
+        k_mat[1][0] * v_x + k_mat[1][1] * v_y + k_mat[1][2] * omega,
+        k_mat[2][0] * v_x + k_mat[2][1] * v_y + k_mat[2][2] * omega,
+        k_mat[3][0] * v_x + k_mat[3][1] * v_y + k_mat[3][2] * omega,
+        k_mat[4][0] * v_x + k_mat[4][1] * v_y + k_mat[4][2] * omega,
+        k_mat[5][0] * v_x + k_mat[5][1] * v_y + k_mat[5][2] * omega,
+        k_mat[6][0] * v_x + k_mat[6][1] * v_y + k_mat[6][2] * omega,
+        k_mat[7][0] * v_x + k_mat[7][1] * v_y + k_mat[7][2] * omega,
+    };
+
+    // Convert module x,y matrix to wheel speed and angle
+    for (int i = 0; i < NUMBER_OF_MODULES; i++)
+    {
+        float x = module_states_matrix[i * 2 + 1];
+        float y = module_states_matrix[i * 2];
+        float speed = hypotf(x, y);
+
+        chassis_state->states[i].speed = speed;
+
+        if (speed > 1e-6f)
+        {
+            float angle = atan2f(y, x);
+            chassis_state->states[i].angle = angle;
+            chassis_state->states[i].last_angle = angle;
+        }
+        else
+        {
+            chassis_state->states[i].angle = chassis_state->states[i].last_angle;
+        }
+    }
+}
+
+void desaturate_wheel_speeds(swerve_chassis_state_t *chassis_state, swerve_constants_t *swerve_constants) {
+    float highest_speed = fabsf(chassis_state->states[0].speed);
+    for (int i = 1; i < NUMBER_OF_MODULES; i++) // start from 1 to find the highest speed
+    {
+        highest_speed = fmaxf(highest_speed, fabsf(chassis_state->states[i].speed));
+    }
+
+    if (highest_speed < 0.01f) // avoid division by zero
+    {
+        return;
+    }
+
+    float desaturation_coefficient = fabsf(swerve_constants->max_speed / highest_speed);
+    for (int i = 0; i < NUMBER_OF_MODULES; i++)
+    {
+        chassis_state->states[i].speed = chassis_state->states[i].speed * desaturation_coefficient;
+    }
+}
+
+void optimize_module_angles(swerve_chassis_state_t *chassis_state, float measured_angles[NUMBER_OF_MODULES]) {
+    for (int i = 0; i < NUMBER_OF_MODULES; i++)
+    {
+        if (measured_angles[i] > PI) {
+            measured_angles[i] -= 2 * PI;
+        }
+
+        float angle_diff = chassis_state->states[i].angle - measured_angles[i];
+
+        if (angle_diff >= PI_OVER_2 && angle_diff <= 3 * PI_OVER_2) {
+            chassis_state->states[i].angle -= PI;
+            chassis_state->states[i].speed *= -1.0f;
+        }
+        else if (angle_diff <= -PI_OVER_2 && angle_diff >= -3 * PI_OVER_2) {
+            chassis_state->states[i].angle += PI;
+            chassis_state->states[i].speed *= -1.0f;
+        }
+
+        if (chassis_state->states[i].angle < 0) {
+            chassis_state->states[i].angle += 2 * PI;
+        }
+    }
+}
+
+/* 
+ * Convert the chassis state from m/s to ticks per second
+ * @param chassis_state: the chassis state to convert
+ */
+void convert_to_tps(swerve_chassis_state_t *chassis_state, float wheel_diameter) {
+    for (int i = 0; i < NUMBER_OF_MODULES; i++) {
+        chassis_state->states[i].speed *= 60 / (PI * wheel_diameter); // convert from mps to tps
+    }
+}
